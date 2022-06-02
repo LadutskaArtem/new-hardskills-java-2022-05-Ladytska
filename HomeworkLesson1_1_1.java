42ff8e31bec0e09b8febfc5e80e9de27d56aad51
LadytskaArtem <ladytko86@gmail.com>
2022-06-02 16:55:14 +0300

    Lesson2

--- HomeworkLesson1_1_1.java
+++
@@ -1,6 +0,0 @@
-public class HomeworkLesson1_1_1 {
-    public static void main(String[] args) {
-        String multipleLines = "H\ne\nl\nl\no\n \nW\no\nr\nl\nd";
-        System.out.println(multipleLines);
-    }
-}
