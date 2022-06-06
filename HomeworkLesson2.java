c8e7b79a4bdba755928219575a82a64c60c4a921
LadytskaArtem <ladytko86@gmail.com>
2022-06-06 20:00:20 +0300

    HomeworkLesson2

---
+++ HomeworkLesson2.java
@@ -0,0 +1,10 @@
+public class HomeworkLesson2 {
+    public static void main(String[] args) {
+        int salary = 75000;
+        double taxes = salary * 13 / 100;
+
+        System.out.println(salary - taxes);
+
+
+    }
+}
