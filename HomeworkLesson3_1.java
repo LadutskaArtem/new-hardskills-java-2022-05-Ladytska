7c3308b8955fb34040ed87ec4f43c7ffb9560a25
LadytskaArtem <ladytko86@gmail.com>
2022-06-14 16:27:18 +0300

    add HomeworkLesson3_1

---
+++ HomeworkLesson3_1.java
@@ -0,0 +1,12 @@
+public class HomeworkLesson3_1 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 11, 2, 4, 102, 155, 171, 2990, 123, -2, -3, 18};
+        for (int i = 0; i<numbers.length; i++){
+            if (numbers[i] %2 !=0){
+                System.out.println(numbers[i]);
+            }
+        }
+
+
+    }
+}
---
+++ HomeworkLesson3_3.java
@@ -0,0 +1,10 @@
+public class HomeworkLesson3_3 {
+    public static void main(String[] args) {
+        int[] reverseNumbers = {2, 3, 1, 1, 4};
+        for (int i = reverseNumbers.length-1; i>=0;i--){
+
+            System.out.print(reverseNumbers[i]);
+
+        }
+    }
+}
