1ef79e36ab265fad1233635f29c87bfb5ee2c9fc
LadytskaArtem <ladytko86@gmail.com>
2022-06-16 17:22:35 +0300

    add HomeworkLesson3_9

---
+++ HomeworkLesson3_9.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class HomeworkLesson3_9 {
+    public static void main(String[] args) {
+        int[] numbers = {-3, 100, 23, 0, 12, 33, 4, 15, 4};
+        Arrays.sort(numbers, 0, 9);
+        System.out.println(Arrays.toString(numbers));
+
+    }
+}
