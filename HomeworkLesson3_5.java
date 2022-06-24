0ce678eca4105a6be2e69f2ac34ddf1709998bbb
LadytskaArtem <ladytko86@gmail.com>
2022-06-24 13:41:13 +0300

    add HomeworkLesson3_5

---
+++ HomeworkLesson3_5.java
@@ -0,0 +1,9 @@
+public class HomeworkLesson3_5 {
+    public static void main(String[] args) {
+        String star = "*";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(star);
+            star+="*";
+    }
+    }
+}
